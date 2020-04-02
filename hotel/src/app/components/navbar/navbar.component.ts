f3a465db2f71118ce01f639ab81832ba95a97bc9
szende.tamas76@gmail.com <Flaron761015>
2020-04-02 11:24:36 +0200

    modify navbar with sandwitch-bar

--- hotel/src/app/components/navbar/navbar.component.ts
+++ hotel/src/app/components/navbar/navbar.component.ts
@@ -16,7 +16,7 @@ export class NavbarComponent implements OnInit {
   loggedIn: boolean;
   userRole: string;
   email: string;
-  baseRouterLink: string = '';
+  baseRouterLink: string = 'home';
   lastname: string;
 
   isWindowOpen: boolean;
@@ -43,7 +43,7 @@ export class NavbarComponent implements OnInit {
   logout() {
     this.loggedIn = false;
     this.userRole = null;
-    this.baseRouterLink = 'login';
+    this.baseRouterLink = 'home';
     this.router.navigateByUrl('').then(() => this.loginService.logout()
     );
   }
