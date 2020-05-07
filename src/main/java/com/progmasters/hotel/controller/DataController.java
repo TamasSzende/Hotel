6c44dbb5a6fc579a04133696d820228df7168b13
Pepa <eles.peter.12@gmail.com>
2020-05-07 19:32:22 +0200

    database restore from dump.sql

--- src/main/java/com/progmasters/hotel/controller/DataController.java
+++ src/main/java/com/progmasters/hotel/controller/DataController.java
@@ -9,34 +9,48 @@ import com.progmasters.hotel.domain.Room;
 import com.progmasters.hotel.dto.DataBaseFillerCommand;
 import com.progmasters.hotel.dto.HotelDataCreatorItem;
 import com.progmasters.hotel.dto.RoomCreateItem;
-import com.progmasters.hotel.dto.RoomListItem;
 import com.progmasters.hotel.repository.HotelRepository;
 import com.progmasters.hotel.repository.RoomRepository;
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/admin/fill-database")
 public class DataController {
-    private HotelRepository hotelRepository;
-    private RoomRepository roomRepository;
+    private final HotelRepository hotelRepository;
+    private final RoomRepository roomRepository;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String user;
+
+    @Value("${spring.datasource.password}")
+    private String password;
 
     @Autowired
     public DataController(HotelRepository hotelRepository, RoomRepository roomRepository) {
@@ -44,6 +58,10 @@ public class DataController {
         this.roomRepository = roomRepository;
     }
 
+    @GetMapping("/databaserestore")
+    public ResponseEntity<Void> restoreDatabase() {
+        return restoreDatabaseFromBackupWithJDBC() ? new ResponseEntity<>(HttpStatus.CREATED) : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+    }
 
     @GetMapping
     public ResponseEntity<Void> fillDatabase() {
@@ -85,10 +103,10 @@ public class DataController {
 
         for (HotelDataCreatorItem hotelData : hotelList.getDataListOfHotel()) {
             if (hotelRepository.findByHotelName(hotelData.getName()).isEmpty()) {
-                List<Room>roomList = new ArrayList<>();
+                List<Room> roomList = new ArrayList<>();
                 Hotel tempHotel = new Hotel(hotelData.getName(), hotelData.getPostalCode(),
                         hotelData.getCity(), hotelData.getStreetAddress(),
-                        HotelType.valueOf(hotelData.getHotelType()),roomList,
+                        HotelType.valueOf(hotelData.getHotelType()), roomList,
                         hotelData.getHotelImageUrls(), hotelData.getDescription(),
                         hotelData.getHotelFeatures().stream().map(HotelFeatureType::valueOf).collect(Collectors.toList()),
                         hotelData.getLongitude(), hotelData.getLatitude());
@@ -104,4 +122,57 @@ public class DataController {
         }
         return new ResponseEntity(HttpStatus.CREATED);
     }
+
+
+    public boolean restoreDatabaseFromBackupWithCommandLine() {
+        System.out.println("futok");
+        try {
+            File resourceFile = loadFileFromResources("hotel-2020_05_07-dump.sql");
+            String source = resourceFile.getPath();
+            String executableSQL = "C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysql";
+            String dbName = "hotel";
+            String[] executeCmd = new String[]{executableSQL, "--user=" + user, "--password=" + password, dbName, "-e", " source " + source};
+
+            Process runtimeProcess = Runtime.getRuntime().exec(executeCmd);
+            int processComplete = runtimeProcess.waitFor();
+            return processComplete == 0;
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public boolean restoreDatabaseFromBackupWithJDBC() {
+        File resourceFile = loadFileFromResources("hotel-2020_05_07-dump.sql");
+        String[] commands = readSQLCommandsFromFile(resourceFile);
+        boolean result = false;
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            try (Statement statement = connection.createStatement()) {
+                for (String command : commands) {
+                    statement.execute(command);
+                }
+                result = true;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return result;
+    }
+
+    private String[] readSQLCommandsFromFile(File resourceFile) {
+        StringBuilder queryString = new StringBuilder();
+        try (Scanner scanner = new Scanner(resourceFile)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                queryString.append(line).append("\n");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return queryString.toString().split(";\n");
+    }
+
 }
